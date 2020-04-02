56991b408fa6da20be38ddf557c3ba70e62cf403
aloftus3 <53193690+aloftus3@users.noreply.github.com>
2020-04-02 16:14:27 -0400

    added image sources

--- assignment_2/main.js
+++ assignment_2/main.js
@@ -14,6 +14,15 @@ for (let i = 1; i <= 5; i++) {
         displayedImage.src = src_value.target.src; 
     }
 }
+/* 
+Image Sources:
+pic1 - www.brittanytourism.com
+pic2 - https://www.worldwildlife.org/species/giant-panda
+pic3 - https://www.worldwildlife.org/species/tiger
+pic4 - https://www.mypetwarehouse.com.au/my-pet-blog/expert-pet-advice/can-dogs-eat-raw-eggs
+pic5 - https://en.wikipedia.org/wiki/File:Fernando_noronha.jpg
+
+*/
                                 
 /* Wiring up the Darken/Lighten button */  
 /*MDN source used for base code*/
